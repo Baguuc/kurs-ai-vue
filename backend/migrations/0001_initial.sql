2df161cb264665dcc8150a020b0f84e32d783ffa
Jan Żukowski <janzukowski@proton.me>
2025-05-19 10:36:21 +0200

    update: typed database models

--- backend/migrations/0001_initial.sql
+++ backend/migrations/0001_initial.sql
@@ -19,3 +19,23 @@ CREATE TABLE episodes (
   module_id INTEGER,
   FOREIGN KEY(module_id) REFERENCES modules(id)
 );
+
+CREATE TYPE EPISODE AS (
+  id INTEGER,
+  no TEXT,
+  title TEXT,
+  content TEXT
+);
+
+CREATE TYPE MODULE AS (
+  id INTEGER,
+  no TEXT,
+  title TEXT,
+  episodes EPISODE[]
+);
+
+CREATE TYPE COURSE AS (
+  id INTEGER,
+  name TEXT,
+  modules MODULE[]
+);
