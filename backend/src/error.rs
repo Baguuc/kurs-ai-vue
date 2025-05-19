16d7d569fe588bb1d0998f6f4dc32fb01ff29669
Jan Żukowski <janzukowski@proton.me>
2025-05-19 11:09:37 +0200

    update: typed clorinde errors

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -3,5 +3,9 @@ pub enum Error {
     #[error("Generic {0}")]
     Generic(String),
     #[error(transparent)]
-    IO(#[from] std::io::Error)
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    Query(#[from] clorinde::tokio_postgres::Error),
+    #[error(transparent)]
+    CreatePool(#[from] clorinde::deadpool_postgres::CreatePoolError)
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,10 +4,11 @@ pub mod utils;
 
 use clorinde::deadpool_postgres::{Config, CreatePoolError, Pool, Runtime};
 use clorinde::tokio_postgres::NoTls;
+use prelude::*;
 
 // code to test queries from clorind, don't bothere
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let pool = match create_pool().await {
         Ok(res) => res,
         Err(_) => panic!("Cannot connect to database")
@@ -17,22 +18,28 @@ async fn main() {
         .await
         .unwrap();
     
-    let mut query = clorinde::queries::courses::list_courses();
-    let courses = query
-        .bind(&client)
-        .all()
-        .await
-        .unwrap();
-    
+    let mut query = clorinde::queries::courses::fetch_course();
+    let courses = match query
+        .bind(&client, &2)
+        .one()
+        .await 
+    {
+        Ok(res) => res,
+        Err(err) => return Err(prelude::Error::Query(err)) 
+    };
+
     println!("{:?}", courses);
+    
+    return Ok(());
 }
 
-async fn create_pool() -> Result<Pool, CreatePoolError> {
+async fn create_pool() -> Result<Pool> {
     let mut cfg = Config::new();
     cfg.user = Some(String::from("bagu"));
     cfg.password = Some(String::from("123"));
     cfg.host = Some(String::from("127.0.0.1"));
     cfg.port = Some(5432);
     cfg.dbname = Some(String::from("course_test"));
-    cfg.create_pool(Some(Runtime::Tokio1), NoTls)
+
+    return Ok(cfg.create_pool(Some(Runtime::Tokio1), NoTls)?);
 }
