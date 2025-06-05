702dacbdcfb73c8ab868dab8f5d51208f9818be3
Jan Żukowski <janzukowski@proton.me>
2025-06-05 19:51:56 +0200

    update: moved server configuration to environment variables

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,7 @@ use prelude::*;
 use rocket::http::Header;
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
-
+use dotenv::dotenv;
 pub struct CORS;
 
 #[rocket::async_trait]
@@ -22,7 +22,10 @@ impl Fairing for CORS {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:5173"));
+        let allowed_hosts = dotenv::var("CORS_ALLOWED_HOSTS")
+            .expect("Cannot read CORS_ALLOWED_HOSTS from environment");
+
+        response.set_header(Header::new("Access-Control-Allow-Origin", allowed_hosts));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
@@ -31,6 +34,8 @@ impl Fairing for CORS {
 
 #[rocket::launch]
 async fn rocket() -> _ {
+    dotenv().ok();
+
     let pool = create_pool()
         .await
         .expect("Cannot create the database pool!");
@@ -44,11 +49,29 @@ async fn rocket() -> _ {
 
 async fn create_pool() -> Result<Pool> {
     let mut cfg = Config::new();
-    cfg.user = Some(String::from("bagu"));
-    cfg.password = Some(String::from("123"));
-    cfg.host = Some(String::from("127.0.0.1"));
-    cfg.port = Some(5432);
-    cfg.dbname = Some(String::from("course_test"));
+
+    let username = dotenv::var("POSTGRES_USERNAME")
+        .expect("Cannot read POSTGRES_USERNAME from environment.");
+    
+    let password = dotenv::var("POSTGRES_PASSWORD")
+        .expect("Cannot read POSTGRES_PASSWORD from environment.");
+    
+    let host = dotenv::var("POSTGRES_HOST")
+        .expect("Cannot read POSTGRES_HOST from environment.");
+    
+    let port: u16 = dotenv::var("POSTGRES_PORT")
+        .expect("Cannot read POSTGRES_PORT from environment.")
+        .parse()
+        .expect("Cannot read POSTGRES_PORT, invalid value");
+    
+    let database_name = dotenv::var("POSTGRES_DATABASE")
+        .expect("Cannot read POSTGRES_DATABASE from environment.");
+    
+    cfg.user = Some(username);
+    cfg.password = Some(password);
+    cfg.host = Some(host);
+    cfg.port = Some(port);
+    cfg.dbname = Some(database_name);
 
     return Ok(cfg.create_pool(Some(Runtime::Tokio1), NoTls)?);
 }
